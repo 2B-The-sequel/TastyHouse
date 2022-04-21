8fcb70ff6f3887d85299fb532c25736752fafeb9
Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>
2022-04-21 12:59:53 +0200

    Made FoodMenuUtility class library

---
+++ FoodMenuUtility/Models/Order.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace FoodMenuUtility.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public Order(int Id)
+        {
+            this.Id = Id;
+        }
+    }
+}
