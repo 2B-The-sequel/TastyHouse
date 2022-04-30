cb7c29675e82c86fef2829d09ace63c9cbcafd1b
gubbih <tlha37727@edu.ucl.dk>
2022-04-30 16:35:55 +0200

    Moved file

--- FoodMenuUtility/Persistence/ProductRepo.cs
+++ FoodMenuUtility/Persistence/ProductRepo.cs
@@ -3,11 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace FoodMenuUtility.Persistence
 {
-    public class  ProductRepo
+    public class ProductRepo
     {
         // ======================================================
         // Fields & Props
@@ -17,7 +19,7 @@ namespace FoodMenuUtility.Persistence
         private string CnnStr = Properties.Settings.Default.WPF_Connection;
 
         // ======================================================
-        // Constructor: Adding every Product entity from database to "Products" list.
+        // Constructor: Adding every Content entity from database to "Contents" list.
         // ======================================================
         public ProductRepo()
         {
@@ -25,7 +27,8 @@ namespace FoodMenuUtility.Persistence
             using (SqlConnection connection = new(CnnStr))
             {
                 connection.Open();
-                string values = "Product_id, Name, Price, Type, Image";
+                // Hvis billeder skal være der skal de tilføjes til table og values
+                string values = "Product_id, Name, Price, Type";
                 string table = "Product";
                 string CommandText = $"SELECT {values} FROM {table}";
                 SqlCommand sQLCommand = new(CommandText, connection);
@@ -35,41 +38,129 @@ namespace FoodMenuUtility.Persistence
                     {
                         int id = sqldatareader.GetInt32("Product_id");
                         string name = sqldatareader.GetString("Name");
-                        double Price = sqldatareader.GetDouble("Price");
-                        string Type = sqldatareader.GetString("Type");
-                        byte[] Image = null;
+                        double price = sqldatareader.GetDouble("Price");
+                        string type = sqldatareader.GetString("Type");
 
-
+                        /*
                         if (!Convert.IsDBNull(sqldatareader["Image"]))//crash if null
                         {
                             Image = (byte[])sqldatareader["Image"];
                         }
+                        */
 
-
-                        Product list = (id != -1)
-                            ? new(id, name, Price, Type, Image)
-                            : new(name, Price, Type, Image);
-                        Products.Add(list);
+                        Product product = (id != -1)
+                            ? new(id, name, price, type)
+                            : new(name, price, type);
+                        Products.Add(product);
                     }
                 }
             }
         }
-        public void AddNewProduct()
+
+
+
+
+        // ======================================================
+        // Repository CRUD: Create (Adding entity to database)
+        // ======================================================
+
+        public int Add(Product product)
+        {
+            int result;
+            using (SqlConnection connection = new(CnnStr))
+            {
+                connection.Open();
+                result = product.Id;
+                string Name = product.Name;
+                double ExtraPrice = product.Price;
+                string Type = product.Type;
+                // Hvis der er brug for et billed til det.
+                //byte[] Image = contents.image;
+
+                string table = "Product";
+                string coloumns = "Product_id, Name, Price, Type";
+                string values = "@Product_id, @Name, @Price, @Type";
+                string query =
+                    $"INSERT INTO {table} ({coloumns})" +
+                    $"VALUES ({values})";
+
+                SqlCommand sqlCommand = new(query, connection);
+
+                sqlCommand.Parameters.Add("@Name", SqlDbType.NVarChar).Value = product.Name;
+                sqlCommand.Parameters.Add("@Price", SqlDbType.Float).Value = product.Price;
+                sqlCommand.Parameters.Add("@Type", SqlDbType.NVarChar).Value = product.Type;
+                //sqlCommand.Parameters.Add("@Image", SqlDbType.VarBinary).Value = contents.Image;
+
+                sqlCommand.ExecuteNonQuery();
+            }
+            return result;
+        }
+
+        // ======================================================
+        // Repository CRUD: Read (Reading entity from database)
+        // ======================================================
+
+        // Get all from database
+        public List<Product> GetAll()
+        {
+            return Products;
+        }
+
+        public Product GetById(int id)
+        {
+            Product result = null;
+            foreach (Product product in Products)
+            {
+                if (product.Id.Equals(id))
+                {
+                    result = product;
+                }
+            }
+            return result;
+        }
+        // ======================================================
+        // Repository CRUD: Update (Updating existing entity in database)
+        // ======================================================
+
+        public void Update(Product product)
         {
             using (SqlConnection connection = new(CnnStr))
             {
                 connection.Open();
-                string values = "Name, Price, Type, Image";
-                string table = "Product";
-                string CommandText = $"INSERT INTO {table} ({values}) VALUES (@Name, @Price, @Type, @Image)";
-                SqlCommand sQLCommand = new(CommandText, connection);
-                sQLCommand.Parameters.AddWithValue("@Name", Products[Products.Count - 1].Name);
-                sQLCommand.Parameters.AddWithValue("@Price", Products[Products.Count - 1].Price);
-                sQLCommand.Parameters.AddWithValue("@Type", Products[Products.Count - 1].Type);
-                sQLCommand.Parameters.AddWithValue("@Image", Products[Products.Count - 1].Image);
-                sQLCommand.ExecuteNonQuery();
+                int id = product.Id;
+                string Name = product.Name;
+                double Price = product.Price;
+                string Type = product.Type;
+
+                string table = "Content";
+                string values = $"@{id}, @{Name}, @{Price}, @{Type}";
+                string query =
+                    $"UPDATE {table}" +
+                    $"SET Name = @'{Name}', Price = @'{Price}', Type = @'{Type}' " +
+                    $"WHERE Product_id = {id}";
             }
         }
-        
+        // ======================================================
+        // Repository CRUD: Delete (Delete existing entity from database)
+        // ======================================================
+
+        public void Remove(int id)
+        {
+            foreach (Product PT in Products)
+            {
+                if (PT.Id == id)
+                {
+                    Products.Remove(PT);
+                }
+            }
+            using (SqlConnection connection = new(CnnStr))
+            {
+                connection.Open();
+                string table = "Product";
+                string query = $"DELETE FROM {table} WHERE {id} = Product_id";
+                SqlCommand sqlCommand = new(query, connection);
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
     }
 }
